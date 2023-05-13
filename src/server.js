a07da80d236cdcfe526de4736e8ce9379b5bf246
MonP101 <upngocsv1ms1@gmail.com>
2023-05-13 15:30:10 +0700

    update

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser, { BodyParser } from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initwebRoutes from "./route/web";
+
+require("dotenv").config();
+
+let app = express();
+
+// cấu hình app
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initwebRoutes(app);
+
+let port = process.env.PORT || 8081;
+// nếu chết port 8080 sẽ chạy port 8081
+
+app.listen(port, () => {
+  //callback
+  console.log("runing on the port: " + port);
+});
